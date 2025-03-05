d36a253e7fe3b1d49a7d79bf9769843a02d41dba
Jakub Stankiewicz <272264@student.pwr.edu.pl>
2025-03-05 13:37:08 +0100

    Better func

--- frontend/src/board.py
+++ frontend/src/board.py
@@ -1,3 +1,5 @@
+import random
+
 def create_empty_board(size):
     return [[" " for _ in range(size)] for _ in range(size)]
 
@@ -7,11 +9,11 @@ def display_board(board):
 
 def place_word(board, word, row, col, direction):
     if direction == "horizontal":
-        board[row][col] = "x"
+        board[row][col] = "X"
         for i in range(len(word)):
             board[row][col + i + 1] = word[i]
     elif direction == "vertical":
-        board[row][col] = "x"
+        board[row][col] = "X"
         for i in range(len(word)):
             board[row + i + 1][col] = word[i]
 
@@ -21,20 +23,26 @@ def can_place_word(board, word, row, col, direction):
         if col + len(word) + 1 >= len(board):
             return False
         for i in range(len(word)):
-            if board[row][col + i + 1] != " " and board[row][col + i + 1] != word[i]:
+            if board[row][col + i + 1] != " " and board[row][col + i + 1] != word[i] and board[row][col + i + 1] != "X":
                 return False
     elif direction == "vertical":
         if row + len(word) + 1 >= len(board):
             return False
         for i in range(len(word)):
-            if board[row + i + 1][col] != " " and board[row + i + 1][col] != word[i]:
+            if board[row + i + 1][col] != " " and board[row + i + 1][col] != word[i] and board[row + i + 1][col] != "X":
                 return False
     return True
         
 # Function to check if in any of the existing words we can insert some given word
 def find_similar_words(board, word):
+    
+    # Adding iterating thourgh random cols and rows -> eliminating having definition elements next to each other on 1st row
+    
     for row in range(len(board)):
-        for col in range(len(board)):
+        for _ in range (len(board)):
+            col = random.randint(0, len(board) - 1)
+            # if board[row][col]
+        # for col in range(len(board)):
             if board[row][col] in word:
                 for direction in ["horizontal", "vertical"]:
                     if can_place_word(board, word, row, col, direction):
@@ -47,9 +55,13 @@ with open("./words.txt", "r") as file:
 words.sort(key=len, reverse=True)
 
 # limit word length to 10
-words = [word for word in words if (5 <= len(word) <= 13)]
+words = [word for word in words if (5 <= len(word) <= 10)]
 
-board_size = 50
+# Getting rid off of the words that contain letter "x" in them -> otherwise board will fail to skip possible errors
+words =[word.lower() for word in words if "x" not in word]
+
+
+board_size = 20
 board = create_empty_board(board_size)
 
 first_word = words.pop(0)
@@ -58,9 +70,12 @@ place_word(board, first_word, 0, 0, "horizontal")
 # Iterating through words to create a board
 for word in words:
     row, col, direction = find_similar_words(board, word)
+    
     if row is not None:
         place_word(board, word, row, col, direction)
+        print(word, "passed")
     else: 
+        print(word, "failed")
         pass
 
 display_board(board)
